ef999a6f2c5b773ba4f71acc6cb49a1a283cc202
UnderratedDev <UnderratedDev@users.noreply.github.com>
2018-06-13 21:51:52 -0700

    Update bot.py
    Made it better.

--- bot.py
+++ bot.py
@@ -16,11 +16,14 @@ async def on_ready():
 @bot.command()
 async def whoru():
 	"""Who am i?"""
-	await bot.say("I am Fillmore.")
+	await bot.say("I am Yudhvir.")
 
 try:
 	f = open('token')
 	tok = f.readline().strip()
 	bot.run(tok)
+	print("stop using python")
+	print("love you karl<3")
+	print("this git repo is not very private, ahahahahah")
 except OSError:
 	print("Token file not found!")
